83abc14f9c93ce05971a1c4e96ff6847a6f6bdc5
mhowell9 <136186906+mhowell9@users.noreply.github.com>
2024-02-13 21:01:42 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function yes() {
+    document.getElementById("center-img").src = "images/milk_heart.png"
+    document.getElementById("main-text").innerHTML = "Yay :D"
+}
+
+function no() {
+    document.getElementById("center-img").src = "images/milk_cry.png"
+    document.getElementById("main-text").innerHTML = "oh man :("
+}
